5f9eb3bfa7743041ea1b82892e67084efce1d344
Kogaweidner <164076299+Kogaweidner@users.noreply.github.com>
2024-09-24 11:02:41 +0400

    Add files via upload

---
+++ PythonProject01.py
@@ -0,0 +1,47 @@
+#Single quote
+print('Welcome to CST1510')
+#Double quotes
+print("Welcome to CST1510(02)")
+#Triple quotes for multiline strings
+print('''...
+Welcome
+to 
+CST1510(02)
+...''')
+
+#Enclose a string in single quotes
+print('Welcome to Python!')
+#Printing a comma-separated list of items
+print('Welcome', 'to', 'MDX!')
+#two strings/texts using the + sign
+print('Welcome'+ 'to'+ 'MDX!')
+
+#Sep parameter
+print('25','09','2024', sep='/')
+#2nd example showing a different sign of separator
+print('a.wieclaw','mdx.ac.uk',sep='@')
+#End Parameter how to print without a new line
+print('Welcome to', end='---')
+print('MDX University')
+
+#Backslash in a string acts as an escape character
+print('Welcome\nto\n\nPython!')
+#Insert horizontal tab. using \t
+print('hello\tworld')
+#Insert a backslash character string. using \\
+print('welcome\\to\\MDX')
+#Inster a backslash character string. using \b
+print('Welcomee\btoo\bMDX')
+#space
+print('''
+ ''')
+
+#Text & numbers in print
+#The code below will put the numbers together
+print('40'+'5')
+#This is how to properly add 2 numbers together
+print(40+5)
+#Print signs with numbers
+print('2*'*10)
+#Print couple of times
+print('Hello World\n'*5)
